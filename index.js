f9b8caff8a8a08acadb6d712c0bdace6d7726dd1
anishdahit <120707502+anishdahit@users.noreply.github.com>
2024-05-26 09:33:06 +0545

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function clearErrors(){
 
 }
 function seterror(id, error){
-    //sets error inside tag of id 
+   
     element = document.getElementById(id);
     element.getElementsByClassName('formerror')[0].innerHTML = error;
 
@@ -19,7 +19,7 @@ function validateForm(){
     var returnval = true;
     clearErrors();
 
-    //perform validation and if validation fails, set the value of returnval to false
+    
     var name = document.forms['myForm']["fname"].value;
     if (name.length<5){
         seterror("name", "*Length of name is too short");
@@ -46,7 +46,7 @@ function validateForm(){
     var password = document.forms['myForm']["fpass"].value;
     if (password.length < 6){
 
-        // Quiz: create a logic to allow only those passwords which contain atleast one letter, one number and one special character and one uppercase letter
+        
         seterror("pass", "*Password should be atleast 6 characters long!");
         returnval = false;
     }
